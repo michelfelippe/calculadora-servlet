a140c1fb465b3cb76bfab345a32b9b99b3466819
michelfelippe <63324004+michelfelippe@users.noreply.github.com>
2020-06-02 16:30:05 -0300

    Correção Código

--- src/main/java/br/edu/usj/ads/lpii/calculadora/Calculadora.java
+++ src/main/java/br/edu/usj/ads/lpii/calculadora/Calculadora.java
@@ -6,14 +6,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-
 public class Calculadora {
+    Historico historico = new Historico();
+
+
     @PostMapping(value = "/calcular")
     public ModelAndView postCalcular(@RequestParam String num1, @RequestParam String num2, @RequestParam String operadores){
         ModelAndView modelAndView = new ModelAndView("index");
         Double n1 = Double.parseDouble(num1);
         Double n2 = Double.parseDouble(num2);
         Double resultado = 0.0;
+
+       
         
         //ASPAS DUPLAS PARA FUNCIONAR!
         switch(operadores){
@@ -24,9 +28,13 @@ public class Calculadora {
             default: resultado = 0.00;
         }
 
+        String operacao = num1 + operadores + num2 + " = " + resultado.toString();
+        historico.adicionar(operacao);
+
         String texto = "Resultado:" + resultado;
 
         modelAndView.addObject("msn", texto);
+        modelAndView.addObject("historico", historico.lerOperacoes());
 
         return modelAndView;
     }
